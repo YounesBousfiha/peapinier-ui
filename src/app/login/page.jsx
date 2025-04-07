82d3e2788da677dcfabdd4ae1abfdbdb16ca46e7
Coderyounes <younesanas2021@gmail.com>
2025-04-07 13:20:20 +0100

    first commit

---
+++ src/app/login/page.jsx
@@ -0,0 +1,10 @@
+import { LoginForm } from "@/components/login-form"
+import Layout from "@/app/(components)/layout"
+
+export default function Page() {
+  return (
+      <Layout>
+          <LoginForm />
+      </Layout>
+  );
+}
---
+++ src/app/register/page.jsx
@@ -0,0 +1,10 @@
+
+export default function register() {
+    return (
+        <>
+            <h1>Register Page</h1>
+            <p>Register page content goes here.</p>
+            {/* Add your registration form or other components here */}
+        </>
+    )
+}
