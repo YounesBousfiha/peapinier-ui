c0f7d963dcf8bd350bb0214bd586042816873bab
Coderyounes <younesanas2021@gmail.com>
2025-04-08 14:40:49 +0100

    Add Toaster component to layout
    Integrate the Toaster component for displaying toast notifications. Configured it with position "top-right" and support for up to 99 visible toasts, enhancing user feedback functionality.

--- src/app/(components)/layout.jsx
+++ src/app/(components)/layout.jsx
@@ -1,11 +1,13 @@
 // app/layout.tsx
 import Sidebar from "./SideBar";
 import Navbar from "./NavBar";
+import {Toaster} from "sonner";
 
 export default function Layout({ children }) {
     return (
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+                <Toaster position="top-right" expand={true} visibleToasts={99}/>
                 <Sidebar />
             </div>
             <div className="md:pl-72">
