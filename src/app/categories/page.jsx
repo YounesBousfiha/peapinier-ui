be9264a289e83019c059128d70679571b12a4fdf
Coderyounes <younesanas2021@gmail.com>
2025-04-14 22:07:07 +0100

    Add category management and category creation components
    Integrated `CategoryManagement` for displaying categories and `CategoryAdd` for creating new categories. Updated the layout to include these components and adjusted the structure to improve functionality and user

--- src/app/categories/page.jsx
+++ src/app/categories/page.jsx
@@ -1,18 +1,24 @@
+// page.jsx
 import Layout from "../(components)/layout";
-
-
+import { CategoryManagement } from "../(components)/categoryManagement";
+import CategoryAdd from '../../app/(components)/CategoryAdd';
 export default function CategoryPage() {
     return (
         <Layout>
             <div className="flex flex-col gap-6">
                 <div>
-                    <h1 className="text-3xl font-bold tracking-tight">Category</h1>
+                    <div className="flex justify-between">
+                        <h1 className="text-3xl font-bold tracking-tight">Category</h1>
+                        <CategoryAdd />
+                    </div>
                     <p className="text-muted-foreground">Manage your categories here</p>
                 </div>
-                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-                    {/* Add categories cards here */}
+
+                <div className="w-full overflow-x-auto">
+                    <CategoryManagement />
                 </div>
             </div>
         </Layout>
     );
 }
+
